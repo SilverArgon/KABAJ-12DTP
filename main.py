1edc0b52c8b97e07c2f07cf0af824e2742fa29d0
Ajax Guo <85317418+Pinester@users.noreply.github.com>
2022-05-15 21:04:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# KABAJ via Flask
+# Created by Ajax Guo 11/05
+# Licensed by EspressoForDunfordWare
+from flask import Flask, render_template
+from time import sleep
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template("home.html")
+  #/ h
+
+if __name__ == '__main__':
+  app.run(debug= True, host='0.0.0.0', port=8080)
+# do not execute this code if imported
