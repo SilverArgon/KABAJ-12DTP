da7058a7c8b07393e72d7bd711f37589303c3d57
Ajax Guo <85317418+Pinester@users.noreply.github.com>
2022-05-16 23:46:06 +0000

    good god i dont even know the website doesnt even load anymore oh god oh f-

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template("home.html")
-  #/ h
+
+
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
 
 if __name__ == '__main__':
   app.run(debug= True, host='0.0.0.0', port=8080)
