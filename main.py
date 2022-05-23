d68358a6c81310769b9d2a872db5a6559bb1cb3f
Ajax Guo <85317418+Pinester@users.noreply.github.com>
2022-05-23 00:07:49 +0000

    Header Fixed and works, Sign up Page works

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 # KABAJ via Flask
 # Created by Ajax Guo 11/05
 # Licensed by EspressoForDunfordWare
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from time import sleep
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
 
 
-@app.route('/signup')
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-  return render_template("signup.html")
+    # Requests database to modify to add the details of the newly made account.
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashpassword = generate_password_hash(password)
+        conn = sqlite3.connect("kabaj.db")
+        cursor = conn.cursor()
+        cursor.execute ('''INSERT INTO User
+                        (username, password_hash, image_path)
+                        VALUES (?,?,'blankpfp.png')
+                        ''',(username, hashpassword))
+        conn.commit()
+        conn.close()                
+        return redirect("/")
+    return render_template("signup.html")
 
-if __name__ == '__main__':
-  app.run(debug= True, host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
 # do not execute this code if imported
