6207f04649462e10de03dca641cf14cdb6315c4e
Ajax Guo <85317418+Pinester@users.noreply.github.com>
2022-05-24 22:34:06 +0000

    Login page added

--- main.py
+++ main.py
@@ -33,6 +33,30 @@ def signup():
         return redirect("/")
     return render_template("signup.html")
 
+  
+
+@app.route("/login", methods= ["GET","POST"])
+def login():
+  if request.method == "POST":
+    username = request.form.get("username")
+    password = request.form.get("password")
+    conn = sqlite3.connect("kabaj.db")
+    cursor = conn.cursor()
+    # Line below is just to check if the account exists first 
+    cursor.execute ('''SELECT id FROM User WHERE username = ?''', (username,))
+    user_id = cursor.fetchone()
+    if user_id is None:
+      return render_template("login.html", error = "You do not exist. Stop pretending you do.")
+      # That code above just makes it so that if account is not found then it stops rest of code to execute
+    cursor.execute ('''SELECT password_hash FROM User WHERE id = ?''', (user_id[0],))
+    passfetch =cursor.fetchone()[0]
+    if not check_password_hash(passfetch, password):
+      return render_template("login.html", error = "Password Incorrect.")
+    conn.close()
+    return redirect("/")
+  return render_template("login.html")
+  # Fetches SQL to see if the password matches username. 
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
