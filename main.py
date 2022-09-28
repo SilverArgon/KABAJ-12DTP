d5f760d32949e239cdc392a82ffe5b6f6569b96b
AjaxGuo <>
2022-09-28 20:21:37 +1300

    Pep8 checking , autoincrementing ids
    Also changed board api to post api. going to make proper board api now

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ app = Flask(__name__)
 SECRET_KEY = "184989dcf7e442d9550c4c9e228f9a752cde4045d9ddb19d"
 app.secret_key = SECRET_KEY
 
+
 def get_db():
     # Stores DB connection to a variable. Prevents crashing and db locking.
     if "db" not in g:
         g.db = sqlite3.connect("kabaj.db")
     return g.db
+
 
 # Closes database connection when website goes down
 @app.teardown_appcontext
@@ -31,12 +33,13 @@ def teardown_db(_):
 def is_logged_in():
     # Checks if user is logged in (aka session)
     return session.get("is_logged_in", False)
-   
+
 
 # Home page (wow)
 @app.route("/")
 def home():
     return render_template("home.html", logged_in=is_logged_in())
+
 
 # Signup
 @app.route("/signup", methods=["GET", "POST"])
@@ -55,21 +58,21 @@ def signup():
         if usernamecheck is not None:
             print("test")
             return render_template("signup.html",
-                                   error="Pre-existing account has this username.")
-                                
+                                   error="Existing account has this username.")
         cursor.execute('''INSERT INTO User
                         (username, password_hash, image_path, admin)
                         VALUES (?,?,'blankpfp.png',false)
                         ''', (username, hashpassword))
         conn.commit()
         cursor.execute('''SELECT id FROM User WHERE username = ?''',
-                         (username,))
+                       (username,))
         user_id = cursor.fetchone()
         session["user_id"] = user_id
         conn.close
         session["is_logged_in"] = True
         return redirect("/success")
     return render_template("signup.html")
+
 
 # Login page
 @app.route("/login", methods=["GET", "POST"])
@@ -92,7 +95,13 @@ def login():
         passfetch = cursor.fetchone()[0]
         if not check_password_hash(passfetch, password):
             return render_template("login.html", error="Password Incorrect.")
-        session["is_logged_in"] = True,
+        cursor.execute('''SELECT id FROM User WHERE username = ?''',
+                       (username,))
+        user_id = cursor.fetchone()
+        session["user_id"] = user_id
+        print("Logged in Successfully")
+        conn.close
+        session["is_logged_in"] = True
         return redirect("success")
     return render_template("login.html")
     # Fetches SQL to see if the password matches username.
@@ -105,7 +114,9 @@ def logout():
     session.pop("user_id", None)
     return redirect("/")
 
-# Redirect user here if they sign up/log in successfully. Access is allowed if you have a session.
+
+# Redirect user here if they sign up/log in successfully.
+# Access is allowed if you have a session.
 @app.route("/success")
 def success():
     if is_logged_in():
@@ -116,22 +127,24 @@ def success():
 
 
 # Viewing board ID and checking if user has a session
-@app.route("/board/<int:board_id>", methods=["GET","POST"])
-def board(board_id):
+@app.route("/post/<int:board_id>", methods=["GET", "POST"])
+def post(board_id):
     if is_logged_in:
         conn = get_db()
         cursor = conn.cursor()
         cursor.execute('''SELECT name FROM Board WHERE id = ?''', (board_id,))
         name = cursor.fetchone()
 
-        return render_template("post.html", name=name[0], board_id=board_id)  
+        return render_template("post.html", name=name[0], board_id=board_id)
     else:
         return redirect("/login")
 
 
+# datetime conversion for created_at table
 def posttime():
     posttime = datetime.today().strftime("%H%M%d%m%Y")
     return posttime
+
 
 # When posting gets rerouted to new api
 @app.route("/new_post/<int:board_id>", methods=["POST"])
@@ -149,10 +162,13 @@ def new_post(board_id):
         user_id = session.get("user_id", None)
         print(user_id, board_id)
         cursor.execute('''INSERT INTO POST
-                        (created_at, body, user_id, thread_id) VALUES (?,?,?,?)''', (posttime(), posttext, user_id[0], board_id))
+                        (created_at, body, user_id, thread_id)
+                       VALUES (?,?,?,?)''',
+                       (posttime(), posttext, user_id[0], board_id))
         conn.commit()
-        print ("Post made.")
+        print("Post made.")
         return redirect("/")
+
 
 # This is just error 404
 @app.errorhandler(404)
